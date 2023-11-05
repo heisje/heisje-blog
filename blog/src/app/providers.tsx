bdbecd6110399f004d2370e4f883581be70d61b7
heisje <heeje1996@gmail.com>
2023-11-06 01:35:02 +0900

    다크모드 provider

--- blog/src/app/providers.tsx
+++ blog/src/app/providers.tsx
@@ -1,26 +1,34 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
 import { ReactNode, useEffect, useState } from 'react';
+import styles from '../components/test123.module.css';
 
 interface Props {
   children: ReactNode;
 }
 export default function Providers({ children }: Props) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
+  // const [mounted, setMounted] = useState(false);
+  //
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
+  //
+  // if (!mounted) {
+  //   return <>{children}</>;
+  // }
   const STORAGE_KEY = 'theme';
   const THEMES = ['light', 'dark'] as const;
   const DEFAULT_THEME = 'system';
 
   return (
-    <ThemeProvider attribute={'class'} defaultTheme={DEFAULT_THEME} storageKey={STORAGE_KEY}>
+    <ThemeProvider
+      attribute={'class'}
+      storageKey={STORAGE_KEY}
+      value={{
+        light: 'light',
+        dark: 'dark',
+      }}
+    >
       {children}
     </ThemeProvider>
   );
