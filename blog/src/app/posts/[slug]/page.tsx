f8e9288c2807418a1bfd470e31e92ee313a295c7
heisje <heeje1996@gmail.com>
2023-11-14 03:07:01 +0900

    feature: 동적 SEO

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -3,9 +3,11 @@ import { allPosts } from '@/../.contentlayer/generated';
 import Image from 'next/image';
 import PostComment from '@/app/posts/[slug]/PostComment';
 import { Metadata, ResolvingMetadata } from 'next';
+import * as process from 'process';
+import Favicon from '@/app/favicon.ico';
 
+// 마크다운 파일의 파일명을 기반으로 Slug를 지정합니다.
 export async function generateStaticParams() {
-  // 마크다운 파일의 파일명을 기반으로 Slug를 지정합니다.
   return allPosts.map(({ _raw }): { slug: string } => ({
     slug: _raw.flattenedPath,
   }));
@@ -13,31 +15,30 @@ export async function generateStaticParams() {
 
 type Props = {
   params: { slug: string };
-  // searchParams: { [key: string]: string | string[] | undefined }
 };
 
 export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-  const postId = String(params.slug || '');
+  const postId: string = params.slug;
 
   // 동적 id 파라미터를 통해서 파일명과 동일한 글을 찾아서 리턴합니다.
   const post = allPosts.find(({ _raw }) => {
     return _raw.flattenedPath === postId;
   });
+
   return {
-    title: `${post?.title}`,
-    // openGraph: {
-    //   images: ['/some-specific-page-image.jpg', ...previousImages],
-    // },
+    title: `${post?.title} | heisje Dev`,
+    description: `[${post?.category}] ${post?.description}`,
+    openGraph: {
+      images: [`${post?.thumbnail}`],
+    },
   };
 }
 
-const PostDetailPage = ({ params: { slug } }: any) => {
+const PostDetailPage = ({ params: { slug } }: { params: { slug: string } }) => {
   const postId: string = slug;
 
   // 동적 id 파라미터를 통해서 파일명과 동일한 글을 찾아서 리턴합니다.
-  const post = allPosts.find(({ _raw }) => {
-    return _raw.flattenedPath === postId;
-  });
+  const post = allPosts.find(({ _raw }) => _raw.flattenedPath === postId);
 
   const MDXComponent = useMDXComponent(post?.body.code || '');
 
@@ -52,7 +53,7 @@ const PostDetailPage = ({ params: { slug } }: any) => {
       />
       <MDXComponent />
 
-      <PostComment />
+      {!process?.env?.IS_LOCAL ? <PostComment /> : <div>DevMode</div>}
     </div>
   );
 };
