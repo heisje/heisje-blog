a0e21a628f460b1308797a4a8a63b2d029a26736
heisje <heeje1996@gmail.com>
2023-11-22 21:09:48 +0900

    fix: main css

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,11 +1,25 @@
+'use client';
+import { Suspense } from 'react';
+
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const Delay = async () => {
+//   await delay(5000);
+//   console.log('끝');
+//   // 상태를 업데이트하거나 렌더링 로직을 여기에 추가합니다.
+//   return <div>끝</div>;
+// };
+
 export default function Example() {
   return (
-    <div className={'flex h-48 items-center'}>
-      <div className="mx-auto text-5xl font-extrabold">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          김희제의 개발 블로그
-        </span>
+    <Suspense fallback={<div className={'w-full h-full bg-blue-500'}>로딩중</div>}>
+      {/*<Delay />*/}
+      <div className={'test flex h-48 items-center'}>
+        <div className="mx-auto text-5xl font-extrabold">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            김희제의 개발 블로그
+          </span>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
